ac6b31410d618d7c225346a7b2d7fd6729df33a3
mhamri <mhamriweb@gmail.com>
2018-03-21 17:31:50 +0800

    session files and cache

---
+++ Demo4.SaveSessionInRedis/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Demo4.SaveSessionInRedis.Controllers
+{
+    public class HomeController : Controller
+    {
+        //https://joonasw.net/view/redis-cache-session-store
+        public async Task<IActionResult> Index()
+        {
+            await HttpContext.Session.LoadAsync();
+            //Get value from session
+            string storedValue = HttpContext.Session.GetString("TestValue");
+            if (storedValue == null)
+            {
+                //No value stored, set one
+                storedValue = "set session in home: " + DateTime.Now.ToString("s");
+                HttpContext.Session.SetString("TestValue", storedValue);
+                //Store session data asynchronously
+                await HttpContext.Session.CommitAsync();
+            }
+
+            return View("Index", storedValue);
+        }
+  
+        public IActionResult SetSession()    
+        {    
+            HttpContext.Session.SetString("seperateset","set session in SetSession: " + DateTime.Now.ToString("s"));    
+            return View("Index", "set session is complete!");    
+        }    
+ 
+        public async Task<IActionResult> GetSession()    
+        {    
+            //Load data from distributed data store asynchronously
+            await HttpContext.Session.LoadAsync();
+            var storedValue = HttpContext.Session.GetString("seperateset");    
+            return View("Index", storedValue); 
+        }  
+
+        
+        public IActionResult Error()
+        {
+            return View("Index", "error happened!");
+        }
+    }
+}
---
+++ Demo4.SaveSessionInRedis/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+
+namespace Demo4.SaveSessionInRedis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSession();
+
+            #region How to customize session
+
+            /*
+            services.AddSession(o =>
+            {
+                o.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                o.Cookie.Name = "MySite.Session";
+                o.Cookie.HttpOnly = true;
+            });
+            */
+
+            #endregion
+
+
+            //Add distributed cache service backed by Redis cache
+            services.AddDistributedRedisCache(o =>
+            {
+                o.Configuration = "localhost:6379";
+                o.InstanceName = "saveSessionInRedis";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseSession();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
